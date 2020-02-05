42823b16333b133deb55442aab053cc751e26503
unknown <denshullyar@gmail.com>
2020-02-05 16:11:58 +0200

    some init

--- dist/js/main.js
+++ dist/js/main.js
@@ -9606,6 +9606,20 @@ function initBlock() {
       el: '.swiper-pagination',
       clickable: true
     },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
